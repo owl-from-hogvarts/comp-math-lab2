1f391db1d0125179699c249d076860568f9dc490
Dylan McKay <me@dylanmckay.io>
2020-07-26 00:11:30 +1200

    Import 'std_stub' module from the ruduino crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+//! Defines simple implementations of required language items that `libstd` normally defines.
+
+#![no_std]
+
+#![feature(lang_items)]
+
+#[lang = "eh_personality"]
+#[no_mangle]
+pub unsafe extern "C" fn rust_eh_personality() -> () {
 }
+
+#[panic_handler]
+fn panic(_info: &::core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
