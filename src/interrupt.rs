d98c51c1ea0fba260a6315ee9d6221772e8ec923
owl-from-hogvarts <transftank@mail.ru>
2024-09-22 17:39:43 +0300

    feat: make interrupts respect previouse interrupts state

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -2,11 +2,24 @@
 
 use core::arch::asm;
 use core::prelude::v1::*;
-use core::marker::PhantomData;
+
+use crate::Register;
 
 /// Helper struct that automatically restores interrupts
 /// on drop.
-struct DisableInterrupts(PhantomData<()>);
+enum InterruptsStatus {
+    Enabled,
+    Disabled,
+}
+
+impl From<bool> for InterruptsStatus {
+    fn from(value: bool) -> Self {
+        match value {
+            true => InterruptsStatus::Enabled,
+            false => InterruptsStatus::Disabled,
+        }
+    }
+}
 
 /// Executes a closure, disabling interrupts until its completion.
 ///
@@ -14,24 +27,47 @@ struct DisableInterrupts(PhantomData<()>);
 /// execution.
 #[inline(always)]
 pub fn without_interrupts<F, T>(f: F) -> T
-    where F: FnOnce() -> T
+where
+    F: FnOnce() -> T,
 {
-    let _disabled = DisableInterrupts::new();
+    let _disabled = InterruptsStatus::disable_safe();
     f()
 }
 
-impl DisableInterrupts {
+impl InterruptsStatus {
     #[inline(always)]
-    pub fn new() -> DisableInterrupts {
-        unsafe { asm!("CLI") }
-        DisableInterrupts(PhantomData)
+    pub fn disable_safe() -> InterruptsStatus {
+        use crate::cores::atmega328::SREG;
+        let status: InterruptsStatus = SREG::is_set(SREG::I).into();
+        unsafe { asm!("CLI") };
+        status
     }
 }
 
-impl Drop for DisableInterrupts {
+impl Drop for InterruptsStatus {
     #[inline(always)]
     fn drop(&mut self) {
-        unsafe { asm!("SEI") }
+        if let InterruptsStatus::Enabled = self {
+            // blink(10);
+            unsafe { asm!("SEI") };
+        }
     }
 }
 
+use crate::pin::Pin;
+
+fn blink(amount: u8) {
+    const DURATION_MS: u64 = 100;
+    crate::cores::atmega328::port::B5::set_output();
+    crate::cores::atmega328::port::B5::set_low();
+
+    for _ in 0..amount {
+        crate::cores::atmega328::port::B5::set_high();
+
+        avr_delay::delay_ms(DURATION_MS);
+
+        crate::cores::atmega328::port::B5::set_low();
+
+        avr_delay::delay_ms(DURATION_MS);
+    }
+}
